f35f260ed4b09d75cfc3b468ac1218539a8df3da
Sourabh Burse <sourabh.burse@gmail.com>
2026-06-04 15:13:48 +0530

    infra: add EF Core migration for supported identity policies

--- ncms-backend/src/NCMS.Backend.Infrastructure/Migrations/20260604094117_AddSupportedIdentityPolicies.Designer.cs
+++ ncms-backend/src/NCMS.Backend.Infrastructure/Migrations/20260604094117_AddSupportedIdentityPolicies.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NCMS.Backend.Core.Entities;
 using NCMS.Backend.Infrastructure.Data;
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NCMS.Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(NcmsDbContext))]
-    partial class NcmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260604094117_AddSupportedIdentityPolicies")]
+    partial class AddSupportedIdentityPolicies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ncms-backend/src/NCMS.Backend.Infrastructure/Migrations/20260604094117_AddSupportedIdentityPolicies.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NCMS.Backend.Core.Entities;
+
+#nullable disable
+
+namespace NCMS.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupportedIdentityPolicies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<ProductIdentityPolicy>>(
+                name: "SupportedIdentityPolicies",
+                table: "Products",
+                type: "jsonb",
+                nullable: false,
+                defaultValueSql: "'[]'::jsonb");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupportedIdentityPolicies",
+                table: "Products");
+        }
+    }
+}
